9d87182026f3351407409d4f0c597fef039dfb6b
mijo <mijo@ailleron.com>
2018-04-11 00:23:54 +0200

    Polishing

--- src/main/java/pl/edu/uj/cenuj/security/SecurityConfiguration.java
+++ src/main/java/pl/edu/uj/cenuj/security/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package pl.edu.uj.cenuj.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
