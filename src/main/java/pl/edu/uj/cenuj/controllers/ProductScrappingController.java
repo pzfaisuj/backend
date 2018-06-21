e8e52a8f4a5ff78b0521c3e3c725e9dfc3c643b9
mijo <mijo@ailleron.com>
2018-06-21 21:02:14 +0200

    ScrapController endpoint's renamed

--- src/main/java/pl/edu/uj/cenuj/controllers/ProductScrappingController.java
+++ src/main/java/pl/edu/uj/cenuj/controllers/ProductScrappingController.java
@@ -2,12 +2,17 @@ package pl.edu.uj.cenuj.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.edu.uj.cenuj.model.scrapping.ScrapData;
+import pl.edu.uj.cenuj.model.scrapping.ScrappedProductLinks;
 import pl.edu.uj.cenuj.services.IProductScrapService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,15 +25,17 @@ public class ProductScrappingController {
         this.productScrapService = productScrapService;
     }
 
-    @GetMapping("/products-to-scrap")
+    @GetMapping("/scrap")
     public List<ScrapData> getProductHistoryByProductId() {
         final List<ScrapData> productsToScrap = productScrapService.getProductsToScrap();
         log.info("getting products to scrap, got: {} domains", productsToScrap);
         return productsToScrap;
     }
 
-    @PostMapping
-    public void addNewProducts() {
-
+    @PostMapping("/scrap")
+    public ResponseEntity<String> addNewProducts(@RequestBody @Valid List<ScrappedProductLinks> scrappedProductLinks) {
+        log.info("got products to scrap");
+        productScrapService.addProductsHistory(scrappedProductLinks);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("product history will be processed");
     }
 }
